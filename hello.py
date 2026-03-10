e15aaac47ec1ae0782838539cc7f455741e5f704
Aman-SK11 <amansheikh5321@gmail.com>
2026-03-10 10:47:25 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+# Simple Python Program
+# First GitHub Project
+
+def greet(name):
+    print("Hello,", name)
+    print("Welcome to my first GitHub Python project!")
+
+name = input("Enter your name: ")
+greet(name)
+
+print("Have a great day!")
